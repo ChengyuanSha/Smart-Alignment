d4cd90e6ba8212e85d81b997a465b353854ccf47
ChengyuanSha <15cs69@queensu.ca>
2021-04-16 16:42:20 -0400

    update read_fasta and BLOSUM62

--- algorithms/utils.py
+++ algorithms/utils.py
@@ -2,27 +2,35 @@
 utils file:
 Common functions used by questions
 """
+import pandas as pd
 
 
 def read_fasta(fname):
-    header = ''
-    for line in fname:
-        if line.startswith('>'):
-            if header:
-                yield header, dna
-            header, dna = line[1:].rstrip().split()[0], ''
-        elif header:
-            dna += line.rstrip()
-    if header:
-        yield header, dna
+    '''
+    Read in a Fasta file and returns a list of sequences.
+    '''
+    temp = []
+    seqs = []
+
+    with open(fname, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('>'):
+                temp.append(line[1:].strip())
+                seqs.append('')
+            else:
+                seqs[-1] += line.strip()
+
+    return seqs
 
 
-def get_dna(fname):
-    with open(fname) as f:
-        fasta_as_list = []
-        for i in f:
-            fasta_as_list.append(i)
-    just_dna = []
-    for line in read_fasta(fasta_as_list):
-        just_dna.append(line[1])
-    return just_dna
+def read_score_matrix(fname):
+    '''
+    Read a text file of a scoring matrix.
+    Return a score matrix dataframe with row and column as amino acids.
+    '''
+    return pd.read_csv(fname, delimiter=r"\s+", header=0, index_col=0)
+
+
+def BLOSUM62():
+    '''Return a BLOSUM62 score matrix dataframe '''
+    return read_score_matrix('../datasets/BLOSUM62.txt')
