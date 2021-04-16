1755c297a83f12aa4fcd766fe3a94a986933aaa1
Tristan Samis <13ts40@queensu.ca>
2021-04-16 12:41:07 -0400

    added LCSM.py, but not all unit tests for now. Util file now can read FASTA input

---
+++ algorithms/LCSM.py
@@ -0,0 +1,16 @@
+import utils
+
+
+def main_LCSM(fname):
+    dataset = utils.get_dna(fname)
+    substr = ''
+    if len(dataset) > 1 and len(dataset[0]) > 0:
+        for i in range(len(dataset[0])):
+            for j in range(len(dataset[0]) - i + 1):
+                if j > len(substr) and all(dataset[0][i:i + j] in x for x in dataset):
+                    substr = dataset[0][i:i + j]
+    return substr
+
+
+if __name__ == '__main__':
+    print(main_LCSM('../datasets/LCSM_1.txt'))  # pragma: no cover
