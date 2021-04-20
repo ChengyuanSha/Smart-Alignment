92de0e44880aa91215722c99bfe4625eab36416b
ChengyuanSha <15cs69@queensu.ca>
2021-04-19 23:53:32 -0400

    fix several bugs in gaff and extension

--- algorithms/GAFF.py
+++ algorithms/GAFF.py
@@ -2,26 +2,27 @@ from algorithms.utils import read_fasta, BLOSUM62
 import numpy as np
 
 
-def get_aligned_seq(s, t, L, U, M, trace_L, trace_U, trace_M, i, j):
+def get_aligned_seq(s, t, L, U, M, trace_L, trace_U, trace_M):
     """Traceback best aligned two sequence using table information."""
     s_aligned, t_aligned = s, t
-    scores = [L[i][j], U[i][j], M[i][j]]
-    max_score = max(scores) # M[i][j]
-    trace = scores.index(max_score)
+    scores = [L[-1, -1], U[-1, -1], M[-1, -1]]
+    trace = np.argmax(scores)
     i, j = len(s), len(t)
     # Build alignment via trace
     while i > 0 and j > 0:
         if trace == 0:
-            if trace_L[i][j] == 0: trace = 2
+            if trace_L[i, j] == 0: trace = 2
             i -= 1
             t_aligned = t_aligned[:j] + '-' + t_aligned[j:]
         elif trace == 1:
-            if trace_U[i][j] == 0: trace = 2
+            if trace_U[i, j] == 0: trace = 2
             j -= 1
             s_aligned = s_aligned[:i] + '-' + s_aligned[i:]
         elif trace == 2:
-            if trace_M[i][j] == 1: trace = 0
-            elif trace_M[i][j] == 2: trace = 1
+            if trace_M[i, j] == 1:
+                trace = 0
+            elif trace_M[i, j] == 2:
+                trace = 1
             else:
                 i -= 1
                 j -= 1
@@ -30,7 +31,7 @@ def get_aligned_seq(s, t, L, U, M, trace_L, trace_U, trace_M, i, j):
         t_aligned = t_aligned[:0] + '-' + t_aligned[0:]
     for _ in range(j):
         s_aligned = s_aligned[:0] + '-' + s_aligned[0:]
-    return max_score, s_aligned, t_aligned
+    return np.amax(scores), s_aligned, t_aligned
 
 
 def main_GAFF(s, t, scoring_matrix, gap, gap_ext):
@@ -44,35 +45,34 @@ def main_GAFF(s, t, scoring_matrix, gap, gap_ext):
     # initializations
     neg_infinity = -999999
     row, col = len(s) + 1, len(t) + 1
-    M = np.zeros((row, col), dtype=int) # main table
-    L = np.full((row, col), neg_infinity, dtype=int) # lower scores
+    M = np.zeros((row, col), dtype=int)  # main table
+    L = np.full((row, col), neg_infinity, dtype=int)  # lower scores
     U = np.full((row, col), neg_infinity, dtype=int)  # upper scores
     trace_M = np.zeros((row, col), dtype=int)
     trace_L = np.zeros((row, col), dtype=int)
     trace_U = np.zeros((row, col), dtype=int)
-    M[1:, 0] = np.arange(gap, gap - M.shape[0] + 1, gap_ext)  # init gap penalty
-    M[0, 1:] = np.arange(gap, gap - M.shape[1] + 1, gap_ext)  # init gap penalty
-    last_i, last_j = 0, 0
+    # init beginning gap penalty
+    M[1:, 0] = np.arange(gap, gap - M.shape[0] * abs(gap_ext) + abs(gap_ext), gap_ext)
+    M[0, 1:] = np.arange(gap, gap - M.shape[1] * abs(gap_ext) + abs(gap_ext), gap_ext)
     # Build the table form top left
     for i in range(1, row):
         for j in range(1, col):
-            costX = [M[i-1][j] + gap, L[i-1][j] + gap_ext]
-            L[i][j] = max(costX)
-            trace_L[i][j] = costX.index(L[i][j])
+            costL = [M[i - 1, j] + gap, L[i - 1, j] + gap_ext]
+            L[i, j] = np.max(costL)
+            trace_L[i, j] = costL.index(L[i, j])
 
-            costY = [M[i][j-1] + gap, U[i][j-1] + gap_ext]
-            U[i][j] = max(costY)
-            trace_U[i][j] = costY.index(U[i][j])
+            costU = [M[i, j - 1] + gap, U[i, j - 1] + gap_ext]
+            U[i, j] = np.max(costU)
+            trace_U[i, j] = costU.index(U[i, j])
 
-            costM = [M[i-1][j-1] + scoring_matrix.loc[s[i-1], t[j-1]],
-                     L[i][j], U[i][j]]
-            M[i][j] = max(costM)
-            trace_M[i][j] = costM.index(M[i][j])
-            last_i, last_j = i, j
-    return get_aligned_seq(s, t, L, U, M, trace_L, trace_U, trace_M, last_i, last_j)
+            costM = [M[i - 1, j - 1] + scoring_matrix.loc[s[i - 1], t[j - 1]],
+                     L[i, j], U[i, j]]
+            M[i, j] = np.max(costM)
+            trace_M[i, j] = costM.index(M[i, j])
+    return get_aligned_seq(s, t, L, U, M, trace_L, trace_U, trace_M)
 
 
 if __name__ == '__main__':
-    s, t = read_fasta('../datasets/GAFF_2.txt') # pragma: no cover
-    alignment = main_GAFF(s, t, BLOSUM62(), -11, -1) # pragma: no cover
-    print('\n'.join(map(str, alignment))) # pragma: no cover
+    s, t = read_fasta('../datasets/GAFF_2.txt')  # pragma: no cover
+    alignment = main_GAFF(s, t, BLOSUM62(), -11, -2)  # pragma: no cover
+    print('\n'.join(map(str, alignment)))  # pragma: no cover
--- algorithms/GAFF_extension.py
+++ algorithms/GAFF_extension.py
@@ -1,12 +1,12 @@
 from algorithms.utils import read_fasta, BLOSUM62
 import numpy as np
 from colorama import Fore, Style
-from algorithms.GAFF import main_GAFF, get_aligned_seq
+from algorithms.GAFF import main_GAFF
 from algorithms.HAMM import main_HAMM
 
 
 def initializations(s, t, scoring_matrix, gap, gap_ext, conserved_seq, conserved_strength,
-                    ignore_start_gaps, ignore_end_gaps):
+                    ignore_start_gaps):
     """Initializations for the main function."""
     neg_infinity = -999999
     row, col = len(s) + 1, len(t) + 1
@@ -16,22 +16,59 @@ def initializations(s, t, scoring_matrix, gap, gap_ext, conserved_seq, conserved
     trace_M = np.zeros((row, col), dtype=int)
     trace_L = np.zeros((row, col), dtype=int)
     trace_U = np.zeros((row, col), dtype=int)
-    if ignore_start_gaps and ignore_end_gaps:
+    if ignore_start_gaps:
         M[1:, 0] = 0
-        M[0, 1:] = 0
-    elif ignore_start_gaps and not ignore_end_gaps:
-        M[1:, 0] = 0
-        M[0, 1:] = np.arange(gap, gap - M.shape[1] + 1, gap_ext)  # init gap penalty
-    elif not ignore_start_gaps and ignore_end_gaps:
-        M[1:, 0] = np.arange(gap, gap - M.shape[0] + 1, gap_ext)
         M[0, 1:] = 0
     else:
-        M[1:, 0] = np.arange(gap, gap - M.shape[0] + 1, gap_ext)  # init gap penalty
-        M[0, 1:] = np.arange(gap, gap - M.shape[1] + 1, gap_ext)  # init gap penalty
+        # init beginning gap penalty
+        M[1:, 0] = np.arange(gap, gap - M.shape[0] * abs(gap_ext) + abs(gap_ext), gap_ext)
+        M[0, 1:] = np.arange(gap, gap - M.shape[1] * abs(gap_ext) + abs(gap_ext), gap_ext)
     last_i, last_j = 0, 0
     for i in conserved_seq:
         scoring_matrix.loc[i, i] = conserved_strength
     return M, L, U, trace_M, trace_U, trace_L, last_i, last_j, row, col
+
+
+def get_aligned_seq_ext(s, t, L, U, M, trace_L, trace_U, trace_M, ignore_end_gaps):
+    """Traceback best aligned two sequence using table information."""
+    s_aligned, t_aligned = s, t
+    L_max = np.max([np.max(L[-1, :]), np.max(L[-1, :])])
+    U_max = np.max([np.max(U[-1, :]), np.max(U[-1, :])])
+    M_max = np.max([np.max(M[-1, :]), np.max(M[-1, :])])
+    scores = [L_max, U_max, M_max]
+    trace = np.argmax(scores)
+    i, j = len(s), len(t)
+    # Build alignment via trace
+    while i > 0 and j > 0:
+        if ignore_end_gaps and j > np.argmax(M[-1, :]):  # add end gap to i
+            j -= 1
+            s_aligned = s_aligned[:i] + '-' + s_aligned[i:]
+        elif ignore_end_gaps and i > np.argmax(M[:, -1]):  # add end gap to j
+            i -= 1
+            t_aligned = t_aligned[:j] + '-' + t_aligned[j:]
+        else:
+            if trace == 0:  # row
+                if trace_L[i][j] == 0: trace = 2
+                i -= 1
+                t_aligned = t_aligned[:j] + '-' + t_aligned[j:]
+            elif trace == 1:  # col
+                if trace_U[i][j] == 0: trace = 2
+                j -= 1
+                s_aligned = s_aligned[:i] + '-' + s_aligned[i:]
+            elif trace == 2:  # diagonal, no need to add
+                if trace_M[i][j] == 1:
+                    trace = 0
+                elif trace_M[i][j] == 2:
+                    trace = 1
+                else:
+                    i -= 1
+                    j -= 1
+    # Check the leading gaps
+    for _ in range(i):
+        t_aligned = t_aligned[:0] + '-' + t_aligned[0:]
+    for _ in range(j):
+        s_aligned = s_aligned[:0] + '-' + s_aligned[0:]
+    return np.argmax(scores), s_aligned, t_aligned
 
 
 def GAFF_extended(s, t, scoring_matrix, gap, gap_ext, conserved_seq, conserved_strength,
@@ -45,24 +82,23 @@ def GAFF_extended(s, t, scoring_matrix, gap, gap_ext, conserved_seq, conserved_s
     """
     M, L, U, trace_M, trace_U, trace_L, last_i, last_j, row, col \
         = initializations(s, t, scoring_matrix, gap, gap_ext, conserved_seq,
-                          conserved_strength, ignore_start_gaps, ignore_end_gaps)
+                          conserved_strength, ignore_start_gaps)
     # Build the table form top left
     for i in range(1, row):
         for j in range(1, col):
-            costX = [M[i - 1][j] + gap, L[i - 1][j] + gap_ext]
-            L[i][j] = max(costX)
-            trace_L[i][j] = costX.index(L[i][j])
+            costL = [M[i - 1, j] + gap, L[i - 1, j] + gap_ext]
+            L[i, j] = np.max(costL)
+            trace_L[i, j] = costL.index(L[i, j])
 
-            costY = [M[i][j - 1] + gap, U[i][j - 1] + gap_ext]
-            U[i][j] = max(costY)
-            trace_U[i][j] = costY.index(U[i][j])
+            costU = [M[i, j - 1] + gap, U[i, j - 1] + gap_ext]
+            U[i, j] = np.max(costU)
+            trace_U[i, j] = costU.index(U[i, j])
 
-            costM = [M[i - 1][j - 1] + scoring_matrix.loc[s[i - 1], t[j - 1]],
-                     L[i][j], U[i][j]]
-            M[i][j] = max(costM)
-            trace_M[i][j] = costM.index(M[i][j])
-            last_i, last_j = i, j
-    return get_aligned_seq(s, t, L, U, M, trace_L, trace_U, trace_M, last_i, last_j)
+            costM = [M[i - 1, j - 1] + scoring_matrix.loc[s[i - 1], t[j - 1]],
+                     L[i, j], U[i, j]]
+            M[i, j] = np.max(costM)
+            trace_M[i, j] = costM.index(M[i, j])
+    return get_aligned_seq_ext(s, t, L, U, M, trace_L, trace_U, trace_M, ignore_end_gaps)
 
 
 def print_in_colour(s, t, scoring_matrix):
@@ -94,12 +130,12 @@ def main_extension(fname, conserved_seq, conserved_strength, scoring_matrix, gap
     diffs = main_HAMM(s_aligned, t_aligned)
     new_s, new_t, num_of_gaps = print_in_colour(s_aligned, t_aligned, scoring_matrix)
     print('Align score', max_score)
-    print('Similarity:', len(s_aligned) - diffs, "/", len(s_aligned), ": ", round(diffs / len(s_aligned) * 100, 2), "%")
+    print('Similarity:', len(s_aligned) - diffs, "/",
+          len(s_aligned), ": ", round(diffs / len(s_aligned) * 100, 2), "%")
     print('Gaps:', num_of_gaps, "/", len(s_aligned), ": ", round(num_of_gaps / len(s_aligned) * 100, 2), "%")
     print(new_s)
     print(new_t)
     print(Style.RESET_ALL)
-    print()
 
 
 def experiment1():
@@ -116,7 +152,8 @@ def experiment1():
     new_s, new_t, num_of_gaps = print_in_colour(s_aligned, t_aligned, BLOSUM62())
     diffs = main_HAMM(s_aligned, t_aligned)
     print('Align score', max_score)
-    print('Similarity:', len(s_aligned) - diffs, "/", len(s_aligned), ": ", round(diffs / len(s_aligned) * 100, 2), "%")
+    print('Similarity:', len(s_aligned) - diffs, "/", len(s_aligned),
+          ": ", round(diffs / len(s_aligned) * 100, 2), "%")
     print('Gaps:', num_of_gaps, "/", len(s_aligned), ": ", round(num_of_gaps / len(s_aligned) * 100, 2), "%")
     print(new_s)
     print(new_t)
@@ -125,10 +162,10 @@ def experiment1():
 def experiment2():
     """Semi-global alignment experiment."""
     # Original alignment
-    main_extension('../datasets/extension_2.txt', 'STAY', 30, BLOSUM62(), -15, -1,
+    main_extension('../datasets/extension_2.txt', 'STAY', 30, BLOSUM62(), -10, -2,
                    ignore_start_gaps=False, ignore_end_gaps=False)
     # Semi-global alignment
-    main_extension('../datasets/extension_2.txt', 'STAY', 30, BLOSUM62(), -8, -1,
+    main_extension('../datasets/extension_2.txt', 'STAY', 30, BLOSUM62(), -15, -2,
                    ignore_start_gaps=True, ignore_end_gaps=True)
 
 
